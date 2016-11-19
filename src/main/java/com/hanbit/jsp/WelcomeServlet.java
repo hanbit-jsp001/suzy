954e400b746faf32265faefb2c7569491dde52c1
HB07-20 <HB07-20@HB07_20>
2016-11-19 17:39:25 +0900

    메세지

---
+++ src/main/java/com/hanbit/jsp/WelcomeServlet.java
@@ -0,0 +1,19 @@
+package com.hanbit.jsp;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			
+		resp.getOutputStream().print("Welcome" + req.getMethod());
+		resp.flushBuffer();
+	}
+	
+}
